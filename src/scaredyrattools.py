94deed7b65989fbefd4495097faf242ab97626b6
Anna Li <annajxli@gmail.com>
2018-02-08 16:53:21 -0500

    added compression

--- src/scaredyrattools.py
+++ src/scaredyrattools.py
@@ -1,4 +1,5 @@
 import os
+import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,16 +16,16 @@ def animal_read(inpath, filename, sheet):
     Uses time as row indexes.
     '''
     filepath = os.path.join(inpath,filename)
-    df = pd.read_excel(filepath,sheetname=sheet,skip_footer=34,index_col=0)
+    df = pd.read_excel(filepath,sheetname=sheet,skip_footer=35,index_col=0)
+    group = df.iloc[33][0]
     animal = df.iloc[32][0]
     context = df.iloc[10][0]
-    starttime = df.iloc[11][0]
     print(filename + " " + sheet + " is " + animal + " in " + context)
 
-    df = pd.read_excel(filepath,sheetname=sheet,skiprows=35,index_col=0,headers=0)
+    df = pd.read_excel(filepath,sheetname=sheet,skiprows=36,index_col=0,headers=0)
     df = df[1:]
     df.replace(to_replace='-',value=0,inplace=True)
-    return(animal,context,starttime,df)
+    return(animal,context,group,df)
 
 def find_tones(df, ntones):
     '''
@@ -118,19 +119,16 @@ def get_means(datadict,timebin, ntones):
     Returns a dataframe of mean velocity of timebin at each tone.
     '''
     meanlist = []
-    stdevlist = []
     i = 1
     while i <= ntones:
         epoch = datadict[i]
         vels = epoch['Velocity']
-        mean = round(vels.mean(),3)
-        stdev = round(vels.stdev(),3)
+        mean = round(np.mean(vels),3)
         meanlist.append(mean)
-        stdevlist.append(stdev)
         i += 1
-    meanSEM = pd.DataFrame([meanlist,stdevlist],columns=[timebin + ' Mean Velocity', timebin + 'SEM'])
-    meanSEM.index = np.arange(1, len(means) + 1)
-    return(meanSEM)
+    means = pd.DataFrame(meanlist,columns=[timebin + ' Mean Velocity'])
+    means.index = np.arange(1, len(meanlist) + 1)
+    return(means)
 
 def get_meds(datadict,timebin, ntones):
     '''
@@ -141,12 +139,28 @@ def get_meds(datadict,timebin, ntones):
     while i <= ntones:
         epoch = datadict[i]
         vels = epoch['Velocity']
-        med = round(vels.median(),3)
+        med = round(np.median(vels),3)
         medlist.append(med)
         i += 1
     meds = pd.DataFrame(medlist,columns=[timebin + ' Median Velocity'])
     meds.index = np.arange(1, len(meds) + 1)
     return(meds)
+
+def get_SEMs(datadict,timebin, ntones):
+    '''
+    Returns a dataframe of median velocity of timebin at each tone.
+    '''
+    SEMlist = []
+    i = 1
+    while i <= ntones:
+        epoch = datadict[i]
+        vels = epoch['Velocity']
+        SEM = round(np.std(vels),3)
+        SEMlist.append(SEM)
+        i += 1
+    SEMs = pd.DataFrame(SEMlist,columns=[timebin + 'SEM'])
+    SEMs.index = np.arange(1, len(SEMs) + 1)
+    return(SEMs)
 
 def get_vels(df, ntones):
     '''
@@ -218,25 +232,29 @@ def find_shock_vels(df, i):
 
     return(sresponse)
 
-def scaredy_read(csv_dir):
+def scaredy_read_FC(csv_dir):
     meancsv = []
+    SEMcsv = []
     medcsv = []
 
     for file in os.listdir(csv_dir):
-        if file.startswith("scaredy-rat-mean-"):
+        if file.startswith("FC-mean-"):
             f = os.path.join(csv_dir, file)
             meancsv.append(f)
-        if file.startswith("scaredy-rat-med-"):
+        if file.startswith("FC-SEM-"):
+            f = os.path.join(csv_dir, file)
+            SEMcsv.append(f)
+        if file.startswith("FC-med-"):
             f = os.path.join(csv_dir,file)
             medcsv.append(f)
-    return(meancsv,medcsv)
+    return(meancsv,SEMcsv,medcsv)
 
-def get_anim(csv):
-    m = re.split('-+', csv)
-    anim = m[3]
+def get_anim(csv, n):
+    m = re.split('[-.]', csv)
+    anim = m[n]
     return(anim)
 
-def concat_data(csvlist,tbin):
+def compress_FC_data(csvlist,tbin):
     '''
     tbins:
     0 = tone
@@ -244,11 +262,9 @@ def concat_data(csvlist,tbin):
     2 = shock
     3 = postshock
     '''
-    if tbin > 3:
-        return("Error: invalid time bin")
     allanims = pd.DataFrame()
     for csv in csvlist:
-        anim = get_anim(csv)
+        anim = get_anim(csv,2)
         df = pd.read_csv(csv,index_col=0).transpose()
 
         tonevels = pd.DataFrame(df.iloc[tbin]).transpose()
@@ -256,3 +272,58 @@ def concat_data(csvlist,tbin):
 
         allanims = pd.concat([allanims,tonevels])
     return(allanims)
+
+def concat_FC_data(means, SEMs, meds, ntones):
+    allData = pd.DataFrame()
+
+    for n in range(ntones):
+        allData = allData.append(means.iloc[:,n])
+        allData = allData.append(SEMs.iloc[:,n])
+        allData = allData.append(meds.iloc[:,n])
+
+    ix = []
+    for n in range(ntones):
+        ix.append('Tone {} Mean'.format(n+1))
+        ix.append('Tone {} SEM'.format(n+1))
+        ix.append('Tone {} Median'.format(n+1))
+
+    allData.index = ix
+    allData = allData.transpose()
+
+    return(allData)
+
+def scaredy_read_FC_max(csv_dir):
+    maxToneCSV = []
+    maxPretoneCSV = []
+    maxShockCSV = []
+    maxPostshockCSV = []
+
+    for file in os.listdir(csv_dir):
+        if file.startswith("FC-tone-max-"):
+            f = os.path.join(csv_dir, file)
+            maxToneCSV.append(f)
+        if file.startswith("FC-pretone-max-"):
+            f = os.path.join(csv_dir, file)
+            maxPretoneCSV.append(f)
+        if file.startswith("FC-shock-max-"):
+            f = os.path.join(csv_dir,file)
+            maxShockCSV.append(f)
+        if file.startswith("FC-shock-max-"):
+            f = os.path.join(csv_dir,file)
+            maxPostshockCSV.append(f)
+    return(maxToneCSV, maxPretoneCSV, maxShockCSV, maxPostshockCSV)
+
+def compress_FC_max_data(csvlist):
+    anims = []
+    maxVels = pd.DataFrame()
+
+    for csv in csvlist:
+        anim = get_anim(csv,4)
+        anims.append(anim)
+
+        df = pd.read_csv(csv,index_col=0).transpose()
+        df = df.iloc[-1,:]
+        maxVels = maxVels.append(df)
+
+    maxVels.index = anims
+    return(maxVels)
